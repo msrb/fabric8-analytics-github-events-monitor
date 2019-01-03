b4178a11844a19b2b6c03d79093fd74ce0a823f7
Martin Sehnoutka <msehnout@redhat.com>
2019-01-03 09:43:51 +0100

    Create messages that will be sent to selinon

--- ghmonitor/backend.py
+++ ghmonitor/backend.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 from abc import ABC, abstractmethod
 from selinon import run_flow
@@ -34,3 +35,32 @@ class SelinonBackend(Backend):
     def notify(self, notification_string):
         init_celery(result_backend=False)
         run_flow(SELINON_FLOW_NAME, notification_string)
+
+
+def create_pr_notification(package, repository, id):
+    notification_dict = {
+        "repository": repository,
+        "package": package,
+        "event": "pull-request",
+        "id": id
+    }
+    return json.dumps(notification_dict)
+
+
+def create_issue_notification(package, repository, id):
+    notification_dict = {
+        "repository": repository,
+        "package": package,
+        "event": "issue",
+        "id": id
+    }
+    return json.dumps(notification_dict)
+
+
+def create_push_notification(package, repository):
+    notification_dict = {
+        "repository": repository,
+        "package": package,
+        "event": "push",
+    }
+    return json.dumps(notification_dict)
