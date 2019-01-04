c80b8786ea997a3cf0346a016cf4575b70c1b33f
Martin Sehnoutka <msehnout@redhat.com>
2019-01-04 09:53:26 +0100

    refactor run.py, so that it can be tested

---
+++ ghmonitor/utils.py
@@ -0,0 +1,35 @@
+from ghmonitor.backend import LoggerBackend, create_pr_notification, create_push_notification
+from ghmonitor.gopkg.translate import translate
+from ghmonitor.monitor import repository_exists, get_list_of_packages
+from ghmonitor.monitor import RepositoryMonitor
+
+
+def create_monitors():
+    packages = get_list_of_packages()
+    repos = list(filter(lambda y: y[1] is not None, map(lambda x: (x, translate(x)), packages)))
+    repos = list(filter(lambda x: repository_exists(x[1]), repos))
+    monitors = list(map(lambda x: RepositoryMonitor(*x), repos))
+
+    return monitors
+
+
+def process_new_events(monitor, backend):
+    new_events = monitor.get_new_events()
+    if new_events is None:
+        return
+
+    new_issues = monitor.new_issues(new_events)
+    if new_issues != set():
+        for issue in new_issues:
+            backend.notify(create_pr_notification(monitor.package, monitor.name, issue.id))
+
+    new_commits = monitor.new_commits(new_events)
+    if new_commits != set():
+        backend.notify(create_push_notification(monitor.package, monitor.name))
+
+    new_prs = monitor.new_pull_requests(new_events)
+    if new_prs != set():
+        for pr in new_prs:
+            backend.notify(create_pr_notification(monitor.package, monitor.name, pr.id))
+
+    monitor.seen_events = new_events
--- run.py
+++ run.py
@@ -3,12 +3,9 @@ import os
 
 from time import sleep
 
-from ghmonitor.monitor import get_auth_header, repository_exists, get_list_of_repos, \
-    get_list_of_packages
-from ghmonitor.monitor import RepositoryMonitor
-from ghmonitor.gopkg.translate import translate
-from ghmonitor.backend import LoggerBackend, create_pr_notification, create_issue_notification, \
-    create_push_notification
+from ghmonitor.monitor import get_auth_header
+from ghmonitor.backend import LoggerBackend, create_pr_notification, create_push_notification
+from ghmonitor.utils import create_monitors, process_new_events
 
 
 logger = logging.getLogger('Monitor')
@@ -25,10 +22,7 @@ if __name__ == "__main__":
 
     # Set up list of repositories
     auth_header = get_auth_header()
-    packages = get_list_of_packages()
-    repos = list(filter(lambda y: y[1] is not None, map(lambda x: (x, translate(x)), packages)))
-    repos = list(filter(lambda x: repository_exists(x[1]), repos))
-    monitors = list(map(lambda x: RepositoryMonitor(*x), repos))
+    monitors = create_monitors()
     logger.info('Monitoring these repositories:')
     for m in monitors:
         logger.info(str(m))
@@ -40,23 +34,6 @@ if __name__ == "__main__":
     while True:
         # Run the monitor forever
         for m in monitors:
-            new_events = m.get_new_events()
-            if new_events is None:
-                continue
+            process_new_events(monitors, backend)
 
-            new_issues = m.new_issues(new_events)
-            if new_issues != set():
-                for issue in new_issues:
-                    backend.notify(create_pr_notification(m.package, m.name, issue.id))
-
-            new_commits = m.new_commits(new_events)
-            if new_commits != set():
-                backend.notify(create_push_notification(m.package, m.name))
-
-            new_prs = m.new_pull_requests(new_events)
-            if new_prs != set():
-                for pr in new_prs:
-                    backend.notify(create_pr_notification(m.package, m.name, pr.id))
-
-            m.seen_events = new_events
         sleep(SLEEP_PERIOD)
