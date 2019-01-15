817c8a97a24edce4c66af537224e8c2b22a99a4d
Michal Srb <michal@redhat.com>
2019-01-15 21:51:13 +0100

    Fix cico_run_pylint.sh

--- cico_run_pylint.sh
+++ cico_run_pylint.sh
@@ -4,7 +4,7 @@ set -ex
 
 prep() {
     yum -y update
-    yum -y install epel-release
+    yum -y install epel-release https://centos7.iuscommunity.org/ius-release.rpm
     yum -y install python36u which
 }
 
